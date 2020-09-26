c18aeffd1e851c0831c65d1d555983976d365c31
Eddie Smith Jr <eddielsjr@gmail.com>
2020-09-26 13:26:56 -0500

    Fix links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -45,7 +45,7 @@ const Footer = ({
           </div>
           <div className="footer-bottom space-between text-xxs invert-order-desktop">
             <FooterNav />
-            <div className="footer-copyright">Made by <a href="https://cruip.com">Cruip</a>. All right reserved</div>
+            <div className="footer-copyright">Made by <a href="https://newwavemediatech.com">New Wave MediaTech</a>. All rights reserved</div>
           </div>
         </div>
       </div>
